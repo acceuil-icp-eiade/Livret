47861c9df074a86c3b434f322b3ca384b4c8754c
Accueil ICP EIADE <sabah6@hotmail.fr>
2026-07-01 11:30:15 +0200

    Add cardiology videos with metadata
    Added multiple cardiology-related video entries with titles, file paths, thumbnails, and keywords.

--- videos.js
+++ videos.js
@@ -1,1 +1,103 @@
+const videos = [
+
+{
+    titre: "TAVI",
+    fichier: "videos/cardiologie/TAVI.mp4",
+    miniature: "miniatures/cardiologie/tavi.png",
+    motsCles: [
+        "tavi",
+        "valve",
+        "aortique",
+        "cardiologie",
+        "interventionnelle",
+        "coronaire",
+        "sapien",
+        "valvulopathie"
+    ]
+},
+    {
+    titre: "MITRACLIP",
+    fichier: "videos/cardiologie/clip_mitral1.mp4",
+    miniature: "miniatures/cardiologie/mitraclip.jpg",
+    motsCles: [
+        "tmvr",
+        "valve",
+        "mitrale",
+        "cardiologie",
+        "interventionnelle",
+        "insuffisance",
+        "transseptale",
+        "valvulopathie"
+    ]
+},
+
+{
+    titre: "Ablation de FA par radiofréquence",
+    fichier: "videos/cardiologie/FA_ablation_catheter.mp4",
+    miniature: "miniatures/cardiologie/fa.jpg",
+    motsCles: [
+        "fa",
+        "fibrillation",
+        "atriale",
+        "ablation",
+        "catheter",
+        "electrophysiologie",
+        "arythmie",
+        "cardiologie",
+        "thermique"
+    ]
+},
+    
+    {
+    titre: "Ablation de FA par électroporation",
+    fichier: "videos/cardiologie/electroporation.mp4",
+    miniature: "miniatures/cardiologie/electroporation.jpg",
+    motsCles: [
+        "fa",
+        "fibrillation",
+        "atriale",
+        "ablation",
+        "catheter",
+        "electrophysiologie",
+        "arythmie",
+        "cardiologie"
+        
+    ]
+},
+    
+    {
+    titre: "Cryoablation de FA",
+    fichier: "videos/cardiologie/cryoablation.mp4",
+    miniature: "miniatures/cardiologie/kryo.png",
+    motsCles: [
+        "fa",
+        "fibrillation",
+        "atriale",
+        "ablation",
+        "catheter",
+        "electrophysiologie",
+        "arythmie",
+        "cardiologie",
+        "thermique",
+        "froid"
+    
+    ]
+    },
+        {
+    titre: "chirurgie d'assistance gauche",
+    fichier: "videos/cardiologie/heartmate.mp4",
+    miniature: "miniatures/cardiologie/heartmate.jpg",
+    motsCles: [
+        "assistance",
+        "gauche",
+        "artificiel",
+        "chirurgie",
+        "pompe",
+        "batterie",
+        "transplantation",
+        "cardiaque"
+    
+    ]
+        }
+];
 
